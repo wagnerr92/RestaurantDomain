05fba168f872d5dc5cc2b70126af98d0929f60ae
Wagner Rodrigues <wagner92dev@gmail.com>
2023-11-27 23:50:01 -0300

    Initial Commit

---
+++ RestaurantDomain/RestaurantDomain.h
@@ -0,0 +1,18 @@
+//
+//  RestaurantDomain.h
+//  RestaurantDomain
+//
+//  Created by Wagner Rodrigues on 27/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RestaurantDomain.
+FOUNDATION_EXPORT double RestaurantDomainVersionNumber;
+
+//! Project version string for RestaurantDomain.
+FOUNDATION_EXPORT const unsigned char RestaurantDomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestaurantDomain/PublicHeader.h>
+
+
